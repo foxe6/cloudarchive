fb69e480c20de61ef701345d374946e5d9572d8b
foxe6-temp <foxe6@protonmail.com>
2020-06-01 07:40:37 +0800

    Automatic Deployment: Unknown Commitment

--- cloudarchive/agent.py
+++ cloudarchive/agent.py
@@ -10,7 +10,6 @@ class IA_Agent(object):
     def upload(self, root: str, item: str) -> None:
         for _, sub_dir, files in os.walk(os.path.join(root, item)):
             for file in files:
-                print(root, os.path.sep.join([item]+sub_dir), file)
                 IA_Broker(self.access, self.secret, self.identifier).upload(root, os.path.sep.join([item]+sub_dir), file)
 
 
